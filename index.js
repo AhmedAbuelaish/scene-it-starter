fe874f90ba9122b1a8cfb5a8b1c0118216e785c3
fatherofbread <ahmedabuelaish@hotmail.com>
2018-11-03 15:05:31 -0500

    scene it part 1 done

---
+++ index.js
@@ -0,0 +1,56 @@
+
+var resultsContainer = document.getElementById('resultsContainer')
+
+document.addEventListener('DOMContentLoaded',function(){
+    resultsContainer.innerHTML = renderMovies(movieData)
+
+    document.getElementById('search-form').addEventListener('input', function(e){
+        e.preventDefault()
+        var searchString = e.target.value.toLowerCase();
+        var filteredData = movieData.filter(function(movie){
+            var foundInName = movie.Title.toLowerCase().indexOf(searchString) > -1;
+			var foundInDate = movie.Year.toLowerCase().indexOf(searchString) > -1;
+			return foundInName || foundInDate;
+        })
+        if (e.target.value == ''){
+            console.log('rendering movies')
+            resultsContainer.innerHTML = renderMovies(movieData)
+        } else {
+            console.log('rendering search')
+            resultsContainer.innerHTML = renderMovies(filteredData) + 
+            `<div class="col-12 text-center text-white-50 m-5 p-3" id="pageDivider">
+            <h2>Other Movies You Might Enjoy</h2>
+            </div>` + 
+            renderMovies(movieData)
+        }
+})
+
+function renderMovies (movies) {
+    var resultsHTML = movies.map(function (currentMovie){
+        var resultsHTML = `
+        <div class="col-4 results">
+        <div class="card bg-dark text-white text-center" style="width: 18rem;">
+            <img class="card-img" src=${currentMovie.Poster} alt=${currentMovie.Title} alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${currentMovie.Title}</h5>
+                <p class="card-text">${currentMovie.Year}</p>
+            </div>
+            <div class="card-footer text-white-50 btn">
+                Save to Collection
+            </div>
+        </div>
+        </div>
+        `
+    return resultsHTML 
+
+    })
+    
+    return resultsHTML.join('')
+
+  }
+
+})
+
+
+
+
